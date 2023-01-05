ef830514633001ad09df3bac8b0a2d4eb970e2d5
m.paglia <m.paglia@mvlabs.it>
2023-01-05 17:55:39 +0100

    detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,14 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./components/tabs/tabs.module').then((m) => m.TabsPageModule),
-  },  {
+  },
+  {
     path: 'filter-modal',
     loadChildren: () => import('./filter-modal/filter-modal.module').then( m => m.FilterModalPageModule)
+  },
+  {
+    path: 'detail/:id',
+    loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
   },
 
 ];
---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FilmDetail } from 'src/model/detailFilm';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+})
+export class DetailPage implements OnInit {
+  item: any = {};
+  isNan: boolean = false;
+  genreArray: [] = [];
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    let film = this.http.get(
+      `https://www.omdbapi.com/?i=${id}&apikey=34b98368`
+    );
+    film.subscribe((film: any) => {
+      this.item = film;
+      if (isNaN(+film.Metascore)) {
+        this.isNan = !this.isNan;
+      }
+      this.genreArray = film.Genre.split(',');
+    });
+
+  }
+  back() {
+    this.location.back();
+  }
+}
